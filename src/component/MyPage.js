fed0fc22d4a56a1e834b628c554783b43aa9a4f4
Zzmilk <Zzmilk@gmail.com>
2020-11-10 18:23:22 +0800

    profile

---
+++ src/component/MyPage.js
@@ -0,0 +1,103 @@
+import React from 'react';
+import { Row, Col, Card } from 'antd';
+import styles from "../app.module.css";
+
+
+function MyPage(props) {
+
+  return <>
+    <div style={{ width: '1024px', margin: '0 auto', marginTop: '20px' }}>
+      <h1>Jonsnow1234</h1>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
+        probare, quae sunt a te dicta? Refert tamen, quo modo.
+      </p>
+
+      <h2>Reviews</h2>
+      <div style={{height: '400px', overflowY: 'auto'}}>
+        <Row>
+          <Col span={8}><b>Tenet</b></Col>
+          <Col span={1} offset={15}><b>4.0</b></Col>
+        </Row>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
+        </p>
+
+        <Row>
+          <Col span={8}><b>Tenet</b></Col>
+          <Col span={1} offset={15}><b>4.0</b></Col>
+        </Row>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
+        </p>
+
+        <Row>
+          <Col span={8}><b>Tenet</b></Col>
+          <Col span={1} offset={15}><b>4.0</b></Col>
+        </Row>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
+        </p>
+
+        <Row>
+          <Col span={8}><b>Tenet</b></Col>
+          <Col span={1} offset={15}><b>4.0</b></Col>
+        </Row>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
+        </p>
+      </div>
+
+
+      <Row>
+        <Col span={8}><h1>WatchList</h1></Col>
+        <Col span={4} offset={12}><b>view & edit watchList</b></Col>
+      </Row>
+
+      <Row justify="space-around">
+        <Col span={7}>
+          <Card
+            hoverable
+            className={styles.wd}
+            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"/>}
+          >
+            <Card.Meta title="Europe Street beat" description="www.instagram.com"/>
+          </Card>
+        </Col>
+        <Col span={7}>
+          <Card
+            hoverable
+            className={styles.wd}
+            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"/>}
+          >
+            <Card.Meta title="Europe Street beat" description="www.instagram.com"/>
+          </Card>
+        </Col>
+        <Col span={7}>
+          <Card
+            hoverable
+            className={styles.wd}
+            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"/>}
+          >
+            <Card.Meta title="Europe Street beat" description="www.instagram.com"/>
+          </Card>
+        </Col>
+      </Row>
+    </div>
+  </>
+}
+
+
+export default MyPage;
--- src/index.js
+++ src/index.js
@@ -6,12 +6,14 @@ import App from './App';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import Login from './component/Login';
 import Detail from './component/Detail';
+import MyPage from './component/MyPage';
 import 'antd/dist/antd.css';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/login" exact={true} component={Login}></Route>
+      <Route path="/myPage" exact={true} component={MyPage}></Route>
       <Route path="/detail/:mid" exact={true} component={Detail}></Route>
       <Route path="/" exact={true} component={App}></Route>
     </Switch>

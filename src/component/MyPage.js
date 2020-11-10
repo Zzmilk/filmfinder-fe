3e0595633bbd9bf1a19a9530deb8bb0e84f94a92
Zzmilk <Zzmilk@gmail.com>
2020-11-10 19:47:38 +0800

    mypage api

--- src/component/MyPage.js
+++ src/component/MyPage.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Row, Col, Card, message } from 'antd';
 import styles from "../app.module.css";
 import api from "../api";
@@ -6,12 +6,14 @@ import api from "../api";
 
 function MyPage(props) {
 
+  const [userInfo, setUserInfo] = useState({});
+
   useEffect(() => {
     api
       .get('/my_page/', {})
       .then(({ data }) => {
         if (data.success) {
-          console.log(data);
+          setUserInfo(data)
         } else {
           message.error(data.msg);
         }
@@ -23,93 +25,44 @@ function MyPage(props) {
 
   return <>
     <div style={{ width: '1024px', margin: '0 auto', marginTop: '20px' }}>
-      <h1>Jonsnow1234</h1>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista
-        probare, quae sunt a te dicta? Refert tamen, quo modo.
-      </p>
-
+      <h1>{ userInfo.username }</h1>
       <h2>Reviews</h2>
       <div style={{height: '400px', overflowY: 'auto'}}>
-        <Row>
-          <Col span={8}><b>Tenet</b></Col>
-          <Col span={1} offset={15}><b>4.0</b></Col>
-        </Row>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
-        </p>
-
-        <Row>
-          <Col span={8}><b>Tenet</b></Col>
-          <Col span={1} offset={15}><b>4.0</b></Col>
-        </Row>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
-        </p>
-
-        <Row>
-          <Col span={8}><b>Tenet</b></Col>
-          <Col span={1} offset={15}><b>4.0</b></Col>
-        </Row>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
-        </p>
-
-        <Row>
-          <Col span={8}><b>Tenet</b></Col>
-          <Col span={1} offset={15}><b>4.0</b></Col>
-        </Row>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed nonne merninisti licere mihi ista probare, quae sunt a te dicta? Refert tamen, quo modo.
-        </p>
+        {
+          (userInfo.top_reviews || []).map(({ movie_name, rating_number, review_comment }) => {
+            return <>
+              <Row>
+                <Col span={8}><b>{ movie_name }</b></Col>
+                <Col span={1} offset={15}><b>{ rating_number }</b></Col>
+              </Row>
+              <p>{ review_comment }</p>
+            </>
+          })
+        }
       </div>
 
 
       <Row>
         <Col span={8}><h1>WatchList</h1></Col>
-        <Col span={4} offset={12}><b>view & edit watchList</b></Col>
+        {/*<Col span={4} offset={12}><b>view & edit watchList</b></Col>*/}
       </Row>
 
-      <Row justify="space-around">
-        <Col span={7}>
-          <Card
-            hoverable
-            className={styles.wd}
-            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"/>}
-          >
-            <Card.Meta title="Europe Street beat" description="www.instagram.com"/>
-          </Card>
-        </Col>
-        <Col span={7}>
-          <Card
-            hoverable
-            className={styles.wd}
-            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"/>}
-          >
-            <Card.Meta title="Europe Street beat" description="www.instagram.com"/>
-          </Card>
-        </Col>
-        <Col span={7}>
-          <Card
-            hoverable
-            className={styles.wd}
-            cover={<img alt="example" src="https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png"/>}
-          >
-            <Card.Meta title="Europe Street beat" description="www.instagram.com"/>
-          </Card>
-        </Col>
+      <Row justify="space-around" style={{marginBottom: '20px', width: '1024px', overflowX: 'auto'}}>
+        {
+          (userInfo.wishlist || []).map(({ name, poster = 'https://os.alipayobjects.com/rmsportal/QBnOOoLaAfKPirc.png' }) => {
+            return <>
+              <Col>
+                <Card
+                  hoverable
+                  className={styles.wd}
+                  cover={<img alt="example" src={poster}/>}
+                >
+                  <Card.Meta title={name} description=""/>
+                </Card>
+              </Col>
+            </>;
+          })
+        }
       </Row>
     </div>
   </>

53de0564e0d04c6e02a9d52f0aa3036bd453646f
Zzmilk <Zzmilk@gmail.com>
2020-11-13 20:32:37 +0800

    change

--- src/component/Cheader.js
+++ src/component/Cheader.js
@@ -24,10 +24,17 @@ function Cheader({ props }) {
   return <Header style={{backgroundImage: 'linear-gradient(50deg, #816EFE,#227DE8)' }}>
     <Row justify="end">
       <Space>
-        <Button type="link" onClick={() => props.history.push('/login')} style={{ color: 'white'}}>Login</Button>
-        <Button type="link" onClick={() => logout(props)} style={{ color: 'white'}}>Logout</Button>
-        <a onClick={ () => props.history.push('/myPage')}><Avatar  style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} /></a>
-        <span style={{ color: 'white' }}>{(JSON.parse(localStorage.getItem('user')) || {}).username}</span>
+        {
+          !localStorage.getItem('user') && <Button type="link" onClick={() => props.history.push('/login')} style={{ color: 'white'}}>Login</Button>
+        }
+        {
+          localStorage.getItem('user') && <>
+            <Button type="link" onClick={() => logout(props)} style={{ color: 'white'}}>Logout</Button>
+            <a onClick={ () => props.history.push('/myPage')}><Avatar  style={{ backgroundColor: '#87d068' }} icon={<UserOutlined />} /></a>
+            <span style={{ color: 'white' }}>{(JSON.parse(localStorage.getItem('user')) || {}).username}</span>
+          </>
+        }
+
       </Space>
     </Row>
   </Header>
